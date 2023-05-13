b38e6236b1fe80a1bbdede7c3600d26a0fee6eb0
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-13 15:24:01 +0800

    Update playlist_lists.h

--- DarwinStreamingSrvr5.5.5-Source/PlaylistBroadcaster.tproj/playlist_lists.h
+++ DarwinStreamingSrvr5.5.5-Source/PlaylistBroadcaster.tproj/playlist_lists.h
@@ -1,28 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-
 #ifndef playlist_lists_H
 #define playlist_lists_H
 
@@ -44,25 +19,13 @@
 #include "playlist_elements.h"
 #include "playlist_utils.h"
 
-
-
-// ************************
-//
 // SOCKET LIST
-//
-// ************************
-
 
 class SocketList : public ArrayList<UDPSocketPair> {
     
 };
 
-// ************************
-//
 // MEDIA STREAM LIST
-//
-// ************************
-
 
 class MediaStreamList : public ArrayList<MediaStream> {
 
@@ -77,12 +40,7 @@ class MediaStreamList : public ArrayList<MediaStream> {
         void MovieEnded(SInt64 endTime);
 };
 
-// ************************
-//
 // SDP MEDIA LIST
-//
-// ************************
-
 
 class SDPMediaList : public ArrayList<TypeMap> {
         
