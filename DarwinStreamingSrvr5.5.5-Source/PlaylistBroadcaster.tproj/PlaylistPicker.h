1232c4f195a247504fc199366a8b2f0b273e8c3e
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-13 14:55:09 +0800

    Update PlaylistPicker.h

--- DarwinStreamingSrvr5.5.5-Source/PlaylistBroadcaster.tproj/PlaylistPicker.h
+++ DarwinStreamingSrvr5.5.5-Source/PlaylistBroadcaster.tproj/PlaylistPicker.h
@@ -1,26 +1,5 @@
 #ifndef __playlist_picker__
 #define __playlist_picker__
-
-/*
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- *
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- *
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- */
-
-
 #include "PLDoubleLinkedList.h"
 #include "SimplePlayListElement.h"
 
@@ -43,12 +22,10 @@ class PlaylistPicker
         char*       PickOne();
         char*       LastPick() { return fLastPick; }
         UInt32      GetNumBuckets() { return mBuckets; }
-/* changed by emil@popwire.com (see relaod.txt for info) */
         UInt32      GetNumMovies() { return mNumToPickFrom; }
 
         bool        mRemoveFlag;
         bool        mStopFlag;
-/* ***************************************************** */
         long*       mPickCounts;
         long        mNumToPickFrom;
         UInt32      mRecentMoviesListSize;
