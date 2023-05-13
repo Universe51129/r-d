9aa473e7da8bb7a64403b9a7d4168a736e0b2bf1
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-13 15:48:16 +0800

    Update XMLPrefsParser.cpp

--- DarwinStreamingSrvr5.5.5-Source/PrefsSourceLib/XMLPrefsParser.cpp
+++ DarwinStreamingSrvr5.5.5-Source/PrefsSourceLib/XMLPrefsParser.cpp
@@ -1,35 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       XMLPrefsParser.cpp
-
-    Contains:   Prototype implementation of XMLPrefsParser object.
-
-
-*/
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -301,7 +269,6 @@ void XMLPrefsParser::AddNewObject( ContainerRef pref )
         pref->AddEmbeddedTag(subObject);
     }
     
-    // we want to fall through from second case above, so this isn't an else
     if (!strcmp(pref->GetTagName(), kObjectList))
     {
         XMLTag* subObject = NEW XMLTag(kObject);
