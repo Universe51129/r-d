9adca416b38a4b07cf327db8d2ceb7e234334966
Universe51129 <91587729+Universe51129@users.noreply.github.com>
2023-05-13 15:39:52 +0800

    Update FilePrefsSource.cpp

--- DarwinStreamingSrvr5.5.5-Source/PrefsSourceLib/FilePrefsSource.cpp
+++ DarwinStreamingSrvr5.5.5-Source/PrefsSourceLib/FilePrefsSource.cpp
@@ -1,35 +1,3 @@
-/*
- *
- * @APPLE_LICENSE_HEADER_START@
- * 
- * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
- * 
- * This file contains Original Code and/or Modifications of Original Code
- * as defined in and that are subject to the Apple Public Source License
- * Version 2.0 (the 'License'). You may not use this file except in
- * compliance with the License. Please obtain a copy of the License at
- * http://www.opensource.apple.com/apsl/ and read it before using this
- * file.
- * 
- * The Original Code and all software distributed under the License are
- * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
- * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
- * Please see the License for the specific language governing rights and
- * limitations under the License.
- * 
- * @APPLE_LICENSE_HEADER_END@
- *
- */
-/*
-    File:       FilePrefsSource.cpp
-
-    Contains:   Implements object defined in FilePrefsSource.h.
-
-    Written by: Chris LeCroy
-*/
-
 #include "FilePrefsSource.h"
 #include <string.h>
 #include <stdio.h>
@@ -124,46 +92,10 @@ int FilePrefsSource::GetValueByIndex(const char* inKey, UInt32 inIndex, char* io
         return false;
     
     return true;
-    
-    /*
-    char* valuePtr = thePair->fValue;
-
-    //this function makes the assumption that fValue doesn't start with whitespace
-    Assert(*valuePtr != '\t');
-    Assert(*valuePtr != ' ');
-    
-    for (UInt32 count = 0; ((count < inIndex) && (valuePtr != '\0')); count++)
-    {
-        //go through all the "words" on this line (delimited by whitespace)
-        //until we hit the one specified by inIndex
-
-        //we aren't at the proper word yet, so skip...
-        while ((*valuePtr != ' ') && (*valuePtr != '\t') && (*valuePtr != '\0'))
-            valuePtr++;
-
-        //skip over all the whitespace between words
-        while ((*valuePtr == ' ') || (*valuePtr == '\t'))
-            valuePtr++;
-        
-    }
-    
-    //We've exhausted the data on this line before getting to our pref,
-    //so return an error.
-    if (*valuePtr == '\0')
-        return false;
-
-    //if we are here, then valuePtr is pointing to the beginning of the right word
-    while ((*valuePtr != ' ') && (*valuePtr != '\t') && (*valuePtr != '\0'))
-        *ioValue++ = *valuePtr++;
-    *ioValue = '\0';
-    
-    return true;
-    */
-}
+   }
 
 char* FilePrefsSource::GetValueAtIndex(UInt32 inIndex)
 {
-    // Iterate through the queue until we have the right entry
     KeyValuePair* thePair = fKeyValueList;
     while ((thePair != NULL) && (inIndex-- > 0))
         thePair = thePair->fNext;
@@ -175,7 +107,6 @@ char* FilePrefsSource::GetValueAtIndex(UInt32 inIndex)
 
 char* FilePrefsSource::GetKeyAtIndex(UInt32 inIndex)
 {
-    // Iterate through the queue until we have the right entry
     KeyValuePair* thePair = fKeyValueList;
     while ((thePair != NULL) && (inIndex-- > 0))
         thePair = thePair->fNext;
@@ -188,9 +119,7 @@ char* FilePrefsSource::GetKeyAtIndex(UInt32 inIndex)
 void FilePrefsSource::SetValue(const char* inKey, const char* inValue)
 {
     KeyValuePair* keyValue = NULL;
-    
-    // If the key/value already exists update the value.
-    // If duplicate keys are allowed, however, add a new entry regardless
+
     if ((!fAllowDuplicates) && ((keyValue = this->FindValue(inKey, NULL)) != NULL))
     {
         keyValue->ResetValue(inValue);
@@ -206,22 +135,15 @@ void FilePrefsSource::SetValue(const char* inKey, const char* inValue)
 
 Bool16 FilePrefsSource::FilePrefsConfigSetter( const char* paramName, const char* paramValue[], void* userData )
 {
-/*
-    static callback routine for ParseConfigFile
-*/
     int     valueIndex = 0;
     
     FilePrefsSource *theFilePrefs = (FilePrefsSource*)userData;
     
     Assert( theFilePrefs );
     Assert(  paramName );
-//  Assert(  paramValue[0] );
-    
-    
-    // multiple values are passed in the paramValue array as distinct strs
+
     while ( paramValue[valueIndex] != NULL )
     {   
-        //qtss_printf("Adding config setting  <key=\"%s\", value=\"%s\">\n", paramName,  paramValue[valueIndex] );
         theFilePrefs->SetValue(paramName, paramValue[valueIndex] );
         valueIndex++;
     }
@@ -232,85 +154,8 @@ Bool16 FilePrefsSource::FilePrefsConfigSetter( const char* paramName, const char
 
 int FilePrefsSource::InitFromConfigFile(const char* configFilePath)
 {
-    /* 
-        load config from specified file.  return non-zero
-        in the event of significant error(s).
-    
-    */
     
     return ::ParseConfigFile( true, configFilePath, FilePrefsConfigSetter, this );
-    
-    /*
-    int err = 0;
-    char bufLine[kMaxLineLen];
-    char key[kMaxValLen];
-    char value[kMaxLineLen];
-    
-    FILE* fileDesc = ::fopen( configFilePath, "r");
-
-    if (fileDesc == NULL)
-    {
-        // report some problem here...
-        err = OSThread::GetErrno();
-        
-        Assert( err );
-    }
-    else
-    {
-    
-        while (fgets(bufLine, sizeof(bufLine) - 1, fileDesc) != NULL)
-        {
-            if (bufLine[0] != '#' && bufLine[0] != '\0')
-            {
-                int i = 0;
-                int n = 0;
-
-                while ( bufLine[i] == ' ' || bufLine[i] == '\t')
-                        { ++i;}
-
-                n = 0;
-                while ( bufLine[i] != ' ' &&
-                         bufLine[i] != '\t' &&
-                         bufLine[i] != '\n' &&
-                         bufLine[i] != '\r' &&
-                         bufLine[i] != '\0' &&
-                         n < (kMaxLineLen - 1) )
-                {
-                    key[n++] = bufLine[i++];
-                }
-                key[n] = '\0';
-
-                while (bufLine[i] == ' ' || bufLine[i] == '\t')
-                {++i;}
-
-                n = 0;
-                while ((bufLine[i] != '\n') && (bufLine[i] != '\0') && 
-                        (bufLine[i] != '\r') && (n < kMaxLineLen - 1))
-                {
-                          value[n++] = bufLine[i++];
-                }
-                value[n] = '\0';
-
-                if (key[0] != '#' && key[0] != '\0' && value[0] != '\0')
-                {
-                    qtss_printf("Adding config setting  <key=\"%s\", value=\"%s\">\n", key, value);
-                    this->SetValue(key, value);
-                }
-                else
-                {
-                        //assert(false);
-                }
-            }
-        }
-
-
-        int closeErr = ::fclose(fileDesc);
-        Assert(closeErr == 0);
-    }
-    
-    return err;
-  */
-    
     
 }
 
